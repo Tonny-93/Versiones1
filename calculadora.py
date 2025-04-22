3ddce984e1c1d9d5a12a9fb57e6e4ae0d91f883b
x4v12002 <xavier.martillo@tecsu.edu.ec>
2025-04-22 18:26:03 -0500

    prueba2

--- calculadora.py
+++ calculadora.py
@@ -5,14 +5,11 @@ def promedio(num1 , num2):
   return ((num1+num2)/2)
 
 
-<<<<<<< HEAD
 def dividir(num1, num2):
   return n1 / n2
-
-=======
 
 print(promedio(n1,n2))
 #print(suma(n1,n2))
 #print(multiplicacion(n1,n2))
 #print(dividir(n1,n2))
->>>>>>> e45ba90a53bb42d422d5aae720e0b1eb6498bb70
+
